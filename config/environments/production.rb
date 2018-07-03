23991313184215e7cefafa09babd2d6df3eeaa16
Jose Flores <alfredo_flores123@yahoo.com>
2018-07-03 19:16:03 +0000

    styled the login page with better spacing and login button, removed remember me check box removed  extra end in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-end
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
