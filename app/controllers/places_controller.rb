8af6a00b187e7d43e76423b007ce4cb69db8465a
Jose Flores <alfredo_flores123@yahoo.com>
2018-07-11 04:50:27 +0000

    set up the comments form but has no url so it doesnt do anything

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,7 @@ end
 
   def show
     @place = Place.find(params[:id])
+    @comment = Comment.new
   end
 
   def edit
@@ -63,3 +64,4 @@ end
     params.require(:place).permit(:name, :description, :address)
 end 
 end 
+
