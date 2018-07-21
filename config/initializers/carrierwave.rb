42018325bb3977fad92a10579abc20e572c0fbb3
Jose Flores <alfredo_flores123@yahoo.com>
2018-07-21 17:23:51 +0000

    aws error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ config.fog_provider ='fog/aws'
 config.fog_credentials = {
   provider:       'AWS',
   aws_access_key_id:   ENV["AKIAIKN2MIYLYYGCPFRQ"],
-  aws_secret_access_key: ENV["6R4Cdu9X1MQ0a30zpHvf1zJI0uMesqFI/LPKsY7,https://073952686572.signin.aws.amazon.com/console"],
+  aws_secret_access_key: ENV["6R4Cdu9X1MQ0a30zpHvf1zJI0uMesqFI/LPKsY7"],
 }
 
 config.fog_directory = ENV["jose-testman-user"]
