c2fa6121c0954283b7d8fb6d1e63f0b76c396fc8
Jose Flores <alfredo_flores123@yahoo.com>
2018-07-21 18:21:23 +0000

    error when uploading photos on page amazon

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
-CarrierWave.configure do |config|
-config.fog_provider ='fog/aws'
-config.fog_credentials = {
-  provider:       'AWS',
-  aws_access_key_id:   ENV["AKIAIKN2MIYLYYGCPFRQ"],
-  aws_secret_access_key: ENV["6R4Cdu9X1MQ0a30zpHvf1zJI0uMesqFI/LPKsY7"],
-}
 
-config.fog_directory = ENV["jose-testman-user"]
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+  aws_access_key_id:     ENV["AWS_ACCESS_KEY"], 
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]                # required
 end
